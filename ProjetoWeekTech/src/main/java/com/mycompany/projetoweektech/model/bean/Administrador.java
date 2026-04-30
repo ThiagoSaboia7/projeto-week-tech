7357ebf86eb0d45ef804a3f885c49e11dae88cae
Joao Victor <joaovictormarcondes28@gmail.com>
2026-04-29 21:11:24 -0300

    feat(bean) Implementação da classe Administrador no pacote model.bean

---
+++ ProjetoWeekTech/src/main/java/com/mycompany/projetoweektech/model/bean/Administrador.java
@@ -0,0 +1,34 @@
+package com.mycompany.projetoweektech.model.bean;
+
+public class Administrador {
+
+    private int idAdministrador;
+    private String email;
+    private String senha;
+
+    // GETTERS E SETTERS
+
+    public int getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(int idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
