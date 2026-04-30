ef2a780e4ada18ce4e437d04725705a28ee13106
Joao-Victorx <joaovictormarcondes28@gmail.com>
2026-04-30 14:16:24 -0300

    feat(bean): Implementação da classe Integrante no pacote model.bean

---
+++ ProjetoWeekTech/src/main/java/com/mycompany/projetoweektech/model/bean/Integrante.java
@@ -0,0 +1,43 @@
+package com.mycompany.projetoweektech.model.bean;
+
+public class Integrante {
+
+    private int idIntegrante;
+    private String nome;
+    private String registroAcademico;
+    private int idProjeto;
+
+    // GETTERS E SETTERS
+
+    public int getIdIntegrante() {
+        return idIntegrante;
+    }
+
+    public void setIdIntegrante(int idIntegrante) {
+        this.idIntegrante = idIntegrante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRegistroAcademico() {
+        return registroAcademico;
+    }
+
+    public void setRegistroAcademico(String registroAcademico) {
+        this.registroAcademico = registroAcademico;
+    }
+
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(int idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+}
